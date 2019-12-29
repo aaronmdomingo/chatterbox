898e654dda9fa4173de26ec9a7bda1cc8d41b411
Aaron Domingo <aaronmdomingo@gmail.com>
2019-12-28 18:08:55 -0800

    added routing to app and fadded a custom class for logo

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import LandingPage from './routes/landing-page/landing-page';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <LandingPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
