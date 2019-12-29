2f4a8e07aa596efea3b983bf0ee34ad6ae7c8e9a
Aaron Domingo <aaronmdomingo@gmail.com>
2019-12-28 18:07:24 -0800

    started building out routes and initial landing page component

---
+++ client/src/components/routes/landing-page/landing-page.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+const LandingPage = (props) => {
+    return (
+        <div className="min-h-screen bg-cover bg-center flex flex-col justify-center items-center bg-gray-300">
+            <div className="w-5/6 sm:w-3/4 md:w-1/2 lg:w-1/3 mx-auto mt-2 flex p-6 bg-white rounded-lg shadow-xl">
+                <div className="w-1/4 h-16 logo bg-contain bg-no-repeat bg-center"></div>
+                <div className="w-3/4 h-16 flex flex-col justify-center">
+                    <h4 className="text-3xl text-center text-black leading-tight">Chatter<span className="text-blue-500" >Box</span></h4>
+                    <p className="text-base text-center text-gray-600 leading-normal">Let's talk!</p>
+                </div>
+            </div>
+            <div className="h-64 w-5/6 sm:w-3/4 md:w-1/2 lg:w-1/3 mt-4 mb-2 flex flex-col items-center justify-around p-6 bg-white rounded-lg shadow-xl">
+                <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold w-1/2 py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                    Register
+                </button>
+                <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold w-1/2 py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                    Login
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default withRouter(LandingPage);
