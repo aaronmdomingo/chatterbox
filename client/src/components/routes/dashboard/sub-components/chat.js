e1fdf3e2ca7507c06937a1a14f9665a278ea1a4e
Aaron Domingo <aaronmdomingo@gmail.com>
2019-12-30 18:31:19 -0800

    added dashboard sub components including chat and form with styling

---
+++ client/src/components/routes/dashboard/sub-components/chat.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Chat = props => {
+    return (
+        <div className="w-11/12 mx-auto mt-2 p-2 bg-white rounded-lg shadow-xl" style={{height: '36rem'}}>
+
+        </div>
+    )
+}
+
+export default Chat;
